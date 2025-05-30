128008fc2f801ae5df6b4567a2c9b0e14cc16181
i5nuc <ip14922@gmail.com>
2025-05-30 13:52:50 +0300

    spring initializr

---
+++ src/main/java/javamid/vitrina/VitrinaApplication.java
@@ -0,0 +1,13 @@
+package javamid.vitrina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javamid/vitrina/VitrinaApplicationTests.java
@@ -0,0 +1,13 @@
+package javamid.vitrina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
